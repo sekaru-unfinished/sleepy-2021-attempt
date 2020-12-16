b6094b530841ad33b801b6f9cfc51057d15415d6
Tudor <sekaru@sleepystudios.net>
2020-12-16 18:27:45 +0000

    dont show videos if reduced-motion

--- src/js/games.js
+++ src/js/games.js
@@ -2,31 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import projects from './projects'
 
-const Games = () => (
-  <ul id='games-container' role='list'>
-    {projects.map((project) => (
-      <li key={project.name} className={`game game--${project.class}`}>
-        <div className='game__content'>
-          {project.video &&
-            <video autoPlay loop muted poster={project.poster}>
-              <source src={project.video} />
-            </video>          
-          }
+const Games = () => {
+  const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)")
+  const reducedMotion = !mediaQuery || mediaQuery.matches
 
-          {!project.video &&
-            <img src={project.poster} alt='' />
-          }
+  return (
+    <ul id='games-container' role='list'>
+      {projects.map((project) => (
+        <li key={project.name} className={`game game--${project.class}`}>
+          <div className='game__content'>
+            {project.video && !reducedMotion &&
+              <video autoPlay loop muted poster={project.poster}>
+                <source src={project.video} />
+              </video>          
+            }
 
-          <div className='caption'>
-            <p className='title'>
-              <a href={project.external ?? `/game?${project.name}`}>{project.name}</a>
-            </p>
-            <p className='desc'>{project.desc}</p>
+            {(!project.video || reducedMotion) &&
+              <img src={project.poster} alt='' />
+            }
+
+            <div className='caption'>
+              <p className='title'>
+                <a href={project.external ?? `/game?${project.name}`}>{project.name}</a>
+              </p>
+              <p className='desc'>{project.desc}</p>
+            </div>
           </div>
-        </div>
-      </li>
-    ))}
-  </ul>
-)
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 ReactDOM.render(<Games />, document.getElementById('games'))
